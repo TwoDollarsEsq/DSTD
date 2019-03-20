fae87553659ed0e3cb5f48e9b960d50ca0646f04
TwoDollarsEsq <two_dollars@icloud.com>
2019-03-20 16:27:54 +0200

    Basic version of declarative namespace.

---
+++ DSTD/DCollection.hpp
@@ -0,0 +1,69 @@
+//
+//  DCollection.hpp
+//  DSTD
+//
+//  'D' stands for 'Declative'. So, it is DeclativeCollection,
+//  declarative std and so on.
+//
+//  Created by Artyom Rudakov on 3/20/19.
+//  Copyright © 2019 Artyom Rudakov. All rights reserved.
+//
+
+#pragma once
+#include <vector>
+#include <queue>
+#include <list>
+
+namespace dstd {
+    
+    template <typename Type, template <typename, typename = std::allocator<Type>> class Container>
+    struct DCollection: Container<Type> {
+        using Container<Type>::Container;
+        
+        template <typename closure>
+        inline void forEach(closure action) {
+            for (Type & item : *this) {
+                action(item);
+            }
+        }
+        
+        template <typename closure>
+        inline DCollection filter(closure shouldInclude) {
+            DCollection filteredCollection;
+            forEach([&](Type & item) {
+                if (shouldInclude(item)) {
+                    filteredCollection.insert(filteredCollection.end(), item);
+                }
+            });
+            return filteredCollection;
+        }
+        
+        template <typename closure>
+        inline Type reduce(Type initialResult, closure nextPartialResult) {
+            forEach([&](Type & item) {
+                initialResult = nextPartialResult(initialResult, item);
+            });
+            return initialResult;
+        }
+        
+        template <typename closure>
+        inline auto map(closure transform) {
+            DCollection<decltype(transform(*(this -> begin)())), Container>
+            mappedCollection;
+            forEach([&](Type & item) {
+                mappedCollection.insert(mappedCollection.end(), transform(item));
+            });
+            return mappedCollection;
+        }
+    };
+    
+    template <typename Type> using vector = DCollection<Type, std::vector>;
+    template <typename Type> using queue  = DCollection<Type, std::queue>;
+    template <typename Type> using list   = DCollection<Type, std::list>;
+    
+    // MARK: Lambda-operators
+    const auto plus           = [](auto i0, auto i1) -> auto { return i0 + i1; };
+    const auto minus          = [](auto i0, auto i1) -> auto { return i0 - i1; };
+    const auto multiplication = [](auto i0, auto i1) -> auto { return i0 * i1; };
+    const auto division       = [](auto i0, auto i1) -> auto { return i0 / i1; };
+}
