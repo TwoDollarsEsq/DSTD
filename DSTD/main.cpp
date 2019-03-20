ddcb24f161bddddca85a82bf2a57ce66b3e560d0
TwoDollarsEsq <two_dollars@icloud.com>
2019-03-20 12:45:25 +0200

    Initial Commit

---
+++ DSTD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSTD
+//
+//  Created by Artyom Rudakov on 3/20/19.
+//  Copyright © 2019 Artyom Rudakov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
